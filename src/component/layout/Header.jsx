11f5cbff09dc9b8dccc12f8d262102a45f906d0d
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2024-01-30 11:07:17 +0300

    updated

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -1,9 +1,11 @@
 import React,{useEffect,useState} from "react";
 import {useSelector} from "react-redux"
+import {useNavigate} from "react-router-dom"
 
 const Header = () => {
   const {user} = useSelector((state)=>state.user?.user)
     const [active,setActive] = useState(false)
+    const navigate = useNavigate()
 
     useEffect(() => {
       window.addEventListener('scroll',()=>{
@@ -17,8 +19,8 @@ const Header = () => {
     console.log(active)
     
   return <>
-  <div className={`${active === true? 'fixed top-0 z-30 left-0 right-0 w-full h-[70px] bg-blue-500' : 'relative'} w-full h-[70px] justify-between bg-blue-500`}>
-  <div className="w-[50px] h-[5opx] rounded-full items-center justify-center flex bg-neutral-500">
+  <div className={`${active === true? 'fixed top-0 z-30 left-0 right-0 w-full h-[70px] bg-blue-500' : 'relative'} w-full h-[70px] justify-between items-center bg-blue-500`}>
+  <div className="w-[50px] h-[50px] rounded-full items-center justify-center flex bg-neutral-500" onClick={()=>navigate("/user-profile")}>
   <h3 className="text-xl text-green-500 text-center">
     {user?.name[0]}
   </h3>
