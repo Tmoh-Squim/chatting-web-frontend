37c6302ca49821d38f061f32839967267da3196f
Tmoh-Squim <122232330+Tmoh-Squim@users.noreply.github.com>
2024-01-30 11:03:08 +0300

    updated

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -1,6 +1,8 @@
 import React,{useEffect,useState} from "react";
+import {useSelector} from "react-redux"
 
 const Header = () => {
+  const {user} = useSelector((state)=>state.user?.user)
     const [active,setActive] = useState(false)
 
     useEffect(() => {
@@ -15,8 +17,12 @@ const Header = () => {
     console.log(active)
     
   return <>
-  <div className={`${active === true? 'fixed top-0 z-30 left-0 right-0 w-full h-[70px] bg-blue-500' : 'relative'}w-full h-[70px] bg-blue-500`}>
-
+  <div className={`${active === true? 'fixed top-0 z-30 left-0 right-0 w-full h-[70px] bg-blue-500' : 'relative'} w-full h-[70px] justify-between bg-blue-500`}>
+  <div className="w-[50px] h-[5opx] rounded-full items-center justify-center flex bg-neutral-500">
+  <h3 className="text-xl text-green-500 text-center">
+    {user?.name[0]}
+  </h3>
+  </div>
   </div>
   </>;
 };
